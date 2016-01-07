52cbdbbed6e7076abc9c02cdc01f361bc2675b2f
albertbahia <albert.bahia1@gmail.com>
2016-01-07 12:17:57 -0500

    twitter interview code challenge added

---
+++ twitterInterview/interface.js
@@ -0,0 +1,100 @@
+// ---Uses ES6 (note let keyword for assigning the array of objects)
+// ---Use whatever you want to implement the user interface.
+// ---Loaded external JS dependencies: [jquery, handlebars, ember-template-compiler, ember.debug, angular, react, lodash].
+
+let channels = [{
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/collections_retinaexplore.png",
+    "backgroundColor": "51c7f7",
+    "channel": "Spotlight: J.Cyrus",
+    "backgroundImageUrl": "https://vines.s3.amazonaws.com/static/api/J.Cyrus.png",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Comedy_trans@2x.png",
+    "backgroundColor": "ff5867",
+    "channel": "Comedy",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/weird_explore_2.png",
+    "backgroundColor": "47f4a9",
+    "channel": "weird",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Animals_trans@2x.png",
+    "backgroundColor": "5082e5",
+    "channel": "Animals",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Art_trans@2x.png",
+    "backgroundColor": "be5fb6",
+    "channel": "Art",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/music_explore.png",
+    "backgroundColor": "7870cc",
+    "channel": "Music",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/dance_explore.png",
+    "backgroundColor": "6db9f4",
+    "channel": "Dance",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Sports_trans@2x.png",
+    "backgroundColor": "548899",
+    "channel": "Sports",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/thedrop_retinaexplore.png",
+    "backgroundColor": "2757aa",
+    "channel": "The Zone",
+    "backgroundImageUrl": "https://vines.s3.amazonaws.com/static/api/thezone_background_3.png",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "http://v.cdn.vine.co/static/api/omg_retinaexplore.png",
+    "backgroundColor": "4f3972",
+    "channel": "OMG",
+    "backgroundImageUrl": "https://vines.s3.amazonaws.com/static/api/omg_celeb_background_2.png",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Food_trans@2x.png",
+    "backgroundColor": "ff794d",
+    "channel": "Food",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/DIY_trans@2x.png",
+    "backgroundColor": "33ccbe",
+    "channel": "DIY",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/Places_trans@2x.png",
+    "backgroundColor": "7870cc",
+    "channel": "Places",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}, {
+    "exploreRetinaIconFullUrl": "https://vine.co/static/images/channels_v2/News_trans@2x.png",
+    "backgroundColor": "cccc52",
+    "channel": "News",
+    "backgroundImageUrl": "",
+    "fontColor": "ffffff"
+}];
+
+// angular.module("explore", []).controller("ExploreController", function ($scope) {
+
+//   $scope.channels = channels;
+
+// });
+
+// ---IMPLEMENT SOLUTION BELOW---
+
+
+
